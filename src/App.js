022aeed37bb1d46e87d56e201c20f69a31ca9468
unknown <ayazali.xwave@gmail.com>
2024-10-31 23:02:12 +0500

    weather-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import WeatherDashboard from './Components/WeatherDashboard/weatherdashboard';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+    <WeatherDashboard />
+  </div>
+  );
+}
+
+export default App;
